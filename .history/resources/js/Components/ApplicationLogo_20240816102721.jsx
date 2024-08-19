f6f5b2fd24fcfc1b3aed198788b5d51157f3f78b
AlaaYou <47208437+AlaaYou@users.noreply.github.com>
2024-08-19 09:55:09 +0100

    delete history

--- .history/resources/js/Components/ApplicationLogo_20240816102721.jsx
+++
@@ -1,5 +0,0 @@
-export default function ApplicationLogo(props) {
-    return (
-        <svg {...props} viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg"> <path d="M100 10a90 90 0 1 0 90 90 90 90 0 0 0 -90-90Zm0 160a70 70 0 1 1 70-70 70 70 0 0 1 -70 70Z" fill="#333" /> <circle cx="60" cy="60" r="10" fill="#fff" /> <circle cx="140" cy="60" r="10" fill="#fff" /> <circle cx="60" cy="140" r="10" fill="#fff" /> <circle cx="140" cy="140" r="10" fill="#fff" /> <path d="M60 60l30 30-30 30" stroke="#333" stroke-width="10" stroke-linecap="round" /> <path d="M140 60l-30 30 30 30" stroke="#333" stroke-width="10" stroke-linecap="round" /> <path d="M60 140l30-30-30-30" stroke="#333" stroke-width="10" stroke-linecap="round" /> <path d="M140 140l-30-30 30-30" stroke="#333" stroke-width="10" stroke-linecap="round" /> </svg>
-    );
-}
--- .history/resources/js/Components/ApplicationLogo_20240816104236.jsx
+++
@@ -1,9 +0,0 @@
-export default function ApplicationLogo(props) {
-    return (
-        <svg {...props} viewBox="0 0 1000 900" xmlns="http://www.w3.org/2000/svg">
-            <path d="M50 150h900v600H50V150z" fill="#333" />
-            <path d="M50 150l450 300L950 150" stroke="#fff" strokeWidth="60" strokeLinecap="round" />
-            <path d="M50 750l450-300L950 750" stroke="#fff" strokeWidth="60" strokeLinecap="round" />
-        </svg>
-    );
-}
